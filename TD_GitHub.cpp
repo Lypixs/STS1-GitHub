deafbfd815381e2d1baf562710d061a130d00c97
Lypixs <evan.seyssiecq@gmail.com>
2022-03-18 09:38:48 +0100

    ajout de fonctions
    Ajout des fonctions "estMajeur" ," exposant", "TVA" et "plusGrand" pour l'étudiant 1

--- TD_GitHub.cpp
+++ TD_GitHub.cpp
@@ -1,11 +1,56 @@
 //Fonctions réalisées par l'étudiant 1 :
+void estMajeur(int age)
+{
+	if (age <= 18)
+	{
+		return true;
+	}
+	else 
+	{
+		return false;
+	}
+	
+}
 
 
+int exposant(int nbre, int exp)
+{
+	int resultat;
+	
+	resultat = pow(nbre, exp);
+	
+	return resultat;
+	
+}
 
 
+float TVA(int prix)
+{
+	float resultat;
+	
+	resultat = prix+(prix*(19.6/100));
+	
+	return resultat;
+	
+}
 
 
-
+int plusGrand(int a, int b, int c)
+{
+	if (a >= b && a >= c)
+	{
+		return a;
+	}
+	else if (b >= a && b >=c)
+	{
+		return b;
+	}
+	else 
+	{
+		return c;
+	}
+	
+}
 
 
 
